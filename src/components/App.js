2ff4e9d55189db0c90b65afe81ca4e7cbb3eb6a7
surfer <xxxx@yahoo.com>
2020-02-26 20:35:36 +0800

    remove test data

--- src/components/App.js
+++ src/components/App.js
@@ -4,36 +4,24 @@ import CardList from './card/CardList';
 import Form from './form/Form';
 
 class App extends React.Component {
-  testData = [
-    {
-      name: "Dan Abramov",
-      avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
-      company: "@facebook"
-    },
-    {
-      name: "Sophie Alpert",
-      avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
-      company: "Humu"
-    },
-    {
-      name: "Sebastian Markbåge",
-      avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
-      company: "Facebook"
-    }
-  ];
-
   constructor(props) {
     super(props);
     this.state = {
-      profiles: this.testData
+      profiles: []
     };
   }
-  
+
+  addNewCard = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
+
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Form />
+        <Form onSubmit={this.addNewCard}/>
         <CardList profiles={this.state.profiles} />
       </div>
     );
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Axios from "axios";
 
 class Form extends React.Component {
     state = {
         username: ""
     }
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(this.state.username);
+        const resp = await Axios.get(`https://api.github.com/users/${this.state.username}`);
+        this.props.onSubmit(resp.data);
     };
 
     render() {
